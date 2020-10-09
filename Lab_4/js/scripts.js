c6e8e824ea787b32c24afad58de67e0187ede110
mominakhan1 <57577974+mominakhan1@users.noreply.github.com>
2020-10-08 22:52:03 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -5,17 +5,18 @@ element.textContent = "Momina Khan's Lab 4";
 
 // 2 Insert two paragraphs into the div with the class "content"
 //  Label each paragraph with a distinct class name
-var p1 = document.createElement("p1");
-var p2 = document.createElement("p2");
-var element = docoument.querySelector('div.content');
+//var p1 = document.createElement("p1");
+//var p2 = document.createElement("p2");
+//var element = docoument.querySelector('div.content');
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-document.getElementById("p1").innerHTML = "My name has" + '10' + "characters.";
+//document.getElementById("p1").innerHTML = "My name has" + '10' + "characters.";
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 
 // 6 Add a new line to your second paragraph
+//p2.innerText =+ "\n";
 
 // 7 Return the final three characters of your last name to that new line
 
