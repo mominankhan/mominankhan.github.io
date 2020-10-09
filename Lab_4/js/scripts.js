7ac6d05f8f7f677c415de44ae9c26be664db06ac
mominakhan1 <57577974+mominakhan1@users.noreply.github.com>
2020-10-09 00:01:22 -0400

    Update scripts.js

--- Lab_4/js/scripts.js
+++ Lab_4/js/scripts.js
@@ -15,23 +15,23 @@ p2.appendChild(paragraph2);
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-var totalCharacters = "My name has" + myfullName.length + "characters.";
+var totalCharacters = "My name has" + myFullName.length + "characters.";
 paragraph1.append(totalCharacters);
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 
 // 6 Add a new line to your second paragraph
-var thirdCharacter = "The third character of my last name is" + myfullName.charAt(2).toUpperCase();
+var thirdCharacter = "The third character of my last name is" + myFullName.charAt(2).toUpperCase();
 paragraph2.append(thirdCharacter);
 
 // 7 Return the final three characters of your last name to that new line
 paragraph2.append("\n");
-var finalthree = "The last three characters in my name are" + myfullName.substring(myfullName.length - 3)
+var finalthree = "The last three characters in my name are" + myFullName.substring(myFullName.length - 3, myFullName.length);
 paragraph2.append(finalthree);
 // 8 Substring your first and last name into two separate variables
-var fullname = myfullName.substring(0,6);
-var lastname = myfullName.substring(7,10);
+var fullname = myFullName.substring(0,6);
+var lastname = myFullName.substring(7,10);
 // 9 Add the total length of your first and last names together
-var namelength = fullname.length + lastname.length; 
+var namelength = Fullname.length + lastname.length; 
 // 10 Display that total next to your name in your header
 document.querySelector("header").append("the length of my first and last name is:" + namelength);
