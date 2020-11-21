4b9c5a713ebece5a0ec59a9fedefb494a8bcbe4f
mominankhan <71394929+mominankhan@users.noreply.github.com>
2020-11-21 10:33:16 -0500

    Update setSessionData.js

--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,10 +1,10 @@
 const main = document.getElementById("main");
 
 function getBusRoute() {
-  let busRoute = ""; // Your code here
+  let busRoute = document.getElementById("busroute").value; // Your code here
 
   if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = ""; // Your code here
+    let busRouteURL = "http://api.umd.io/v0/bus/routes" + busRoute; // Your code here
 
     fetch(busRouteURL)
       .then((response) => {
@@ -12,6 +12,22 @@ function getBusRoute() {
       })
       .then((route) => {
         // YOUR CODE HERE
+        console.log("Here");
+        console.group(route);
+        let title, latMax, latMin, lonMax, lonMin;
+        let message = "";
+        if (typeof route.title !== "undefined") {
+          title = route.title;
+          latMax = route.lat_max;
+          latMin = route.lat_min;
+          lonMax = route.lonMax;
+          lonMin = route.lonMin;
+          message = title + " <br>Lat max : " + latMax + "<br>Lat min : " + latMin + "<br>Lon max : " + lonMax + "<br>Lon min :" + lonMin;
+         } else {
+           message = "No bus information available";
+         }
+         console.log(title);
+         main.innerHTML = "Bus Route Info: " + message; 
       })
       .catch((err) => {
         console.log(err);
