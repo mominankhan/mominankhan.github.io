f74c1790a8e27804d706ba51403866d91db6cd3b
mominankhan <71394929+mominankhan@users.noreply.github.com>
2020-11-21 10:33:09 -0500

    Update getSessionData.js

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,14 @@
 // Get saved data from sessionStorage
-let title = ""; // Your code here
-let lat_max = "";
-let lat_min = "";
-let lon_max = "";
-let lon_min = "";
+let title = sessionStorage.getItem("title"); // Your code here
+let lat_max = sessionStorage.getItem("latMax");
+let lat_min = sessionStorage.getItem("latMin");
+let lon_max = sessionStorage.getItem("lonMax");
+let lon_min = sessionStorage.getItem("lonMin");
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+document.getElementById("title").innerHTML = title;
+document.getElementById("lat_max").innerHTML = latMax;
+document.getElementById("lat_min").innerHTML = latMin;
+document.getElementById("lon_max").innerHTML = lonMax;
+document.getElementById("lon_min").innerHTML = lonMin;
