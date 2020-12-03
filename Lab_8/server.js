679fd9b8ddd3b60bf1e1b05c3487f5f49c8af356
mominankhan <71394929+mominankhan@users.noreply.github.com>
2020-12-02 19:50:29 -0500

    Update server.js

--- Lab_8/server.js
+++ Lab_8/server.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 
 
-// Code here for how to set 'public' as the static folder for express
-let labRoute = require('./public/js/Lab_8')
-
+// Code here for how to set 'public' as the static folder for expres
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 8080
 // Code here to have the app listen on port 8080....please provide a console.log message
-app.listen(PORT, () => {console.log('Server connected at : ', PORT);});
+app.listen(8080, () => {
+    console.log("Server started...listening on PORT 8080.");
+})
